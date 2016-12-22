dc78060bd2951df89e48f995908f3e34aa83120a
Caio Amaral Corrêa <caiobep@me.com>
2016-12-22 01:42:45 -0200

    bootstrap.sh:Manage and install dotfiles

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,23 +4,47 @@ cd "$(dirname "${BASH_SOURCE}")";
 
 git pull origin master;
 
-function doIt() {
-	rsync --exclude ".git/" \
-		--exclude ".DS_Store" \
-		--exclude "bootstrap.sh" \
-		--exclude "README.md" \
-		--exclude "LICENSE" \
-		-avh --no-perms . ~;
-	source ~/.bash_profile;
+function keepBashProfile() {
+	# Changing Default BashProfile
+	mv ~/.bash_profile ~/.bash_profileOld
+	cp ./.bash_profile ~/
+}
+
+function deleteBashProfile(){
+	rm -rf ~/.bash_profile
+	cp ./.bash_profile ~/
+}
+
+function moveDotFiles(){
+	cp ./.bash_config ~/
+	cp ./.gitconfig ~/
+	bash .macos
+}
+
+function installBrewfile(){
+	bash ./install.sh
 }
 
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
-	doIt;
+	keepBashProfile;
+	moveDotFiles;
+	installBrewfile;
 else
 	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
 	echo "";
-	if [[ $REPLY =~ ^[Yy]$ ]]; then
-		doIt;
+	if [[ $REPLY =~ ^[Yy]$ ]];
+	then
+		deleteBashProfile;
+		moveDotFiles;
+		installBrewfile;
+	else
+		keepBashProfile;
+		moveDotFiles;
+		installBrewfile;
 	fi;
 fi;
-unset doIt;
+
+unset deleteBashProfile;
+unset keepBashProfile;
+unset moveDotFiles;
+unset installBrewfile;
